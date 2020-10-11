262b055a3e4bf4c721c6d5d1e40b3b166f1237ca
Anumbrella <email@anumbrella.net>
2020-10-11 15:19:05 +0800

    Update ConfirmSender2.java

--- src/main/java/net/anumbrella/rabbitmq/sender/ConfirmSender2.java
+++ src/main/java/net/anumbrella/rabbitmq/sender/ConfirmSender2.java
@@ -57,11 +57,13 @@ public class ConfirmSender2 {
 
             public void handleNack(long deliveryTag, boolean multiple) throws IOException {
                 System.out.println("Nack, SeqNo: " + deliveryTag + ", multiple: " + multiple);
+                /**
                 if (multiple) {
                     confirmSet.headSet(deliveryTag + 1L).clear();
                 } else {
                     confirmSet.remove(deliveryTag);
                 }
+                **/
             }
         });
 
